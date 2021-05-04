5f5da5d9c5150ddf274dddca512d500fd193e610
quite4work <quite4work@mailo.com>
2021-05-04 16:25:20 +0300

    Fix assert.pageIncludes

--- assert.js
+++ assert.js
@@ -1,8 +1,8 @@
 import { assertStringIncludes } from "https://deno.land/std@0.92.0/testing/asserts.ts";
 
 export async function pageIncludes(url, text) {
-  let resp = await fetch(url);
-  let body = await resp.json();
+  const resp = await fetch(url);
+  const body = await resp.text();
   if (Array.isArray(text)) {
     for (const i of text) {
       assertStringIncludes(body, i, `ur ${url} douesnt include: ${i}`);
